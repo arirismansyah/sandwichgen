4063548f256986e985283f2b7684a231c7cf2499
Ari Rismansyah <rismansyah.ari48@gmail.com>
2023-09-10 19:31:55 +0700

    init images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,62 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Sandwich Generation</title>
+  <link rel="stylesheet" href="/assets_template/css/plugins.css" />
+  <link rel="stylesheet" href="/assets_template/css/style.css" />
+  <link
+    rel="preload"
+    href="/assets_template/css/fonts/thicccboi.css"
+    as="style"
+    onload="this.rel='stylesheet'"
+  />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class="wrapper bg-light">
+  <div class="container py-14 py-md-16">
+    <div class="row gx-lg-8 gx-xl-12 gy-10 align-items-center">
+      <div class="col-lg-7">
+        <figure>
+          <img
+            class="w-auto"
+            src="/images/people1.svg"
+            srcset="/images/people1.svg"
+            alt=""
+          />
+        </figure>
+      </div>
+      <!--/column -->
+      <div class="col-lg-5">
+        <h3 class="display-4 mb-6 pe-xxl-6">Mardiansah</h3>
+        <ul class="progress-list mt-3">
+          <li>
+            <p>Marketing</p>
+            <div class="progressbar line blue" data-value="100" />
+          </li>
+          <li>
+            <p>Strategy</p>
+            <div class="progressbar line yellow" data-value="80" />
+          </li>
+          <li>
+            <p>Development</p>
+            <div class="progressbar line orange" data-value="85" />
+          </li>
+          <li>
+            <p>Data Analysis</p>
+            <div class="progressbar line green" data-value="90" />
+          </li>
+        </ul>
+        <!-- /.progress-list -->
+      </div>
+      <!--/column -->
+    </div>
+    <!--/.row -->
+  </div>
+  <!-- /.container -->
 </section>
 
+<!-- /section -->
+
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
