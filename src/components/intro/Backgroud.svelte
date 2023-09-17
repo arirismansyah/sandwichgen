ccbab2cd8563216a19ae7bc88a3829e912584a41
Ari Rismansyah <rismansyah.ari48@gmail.com>
2023-09-17 07:49:49 +0700

    feature: saos svelte

---
+++ src/components/intro/Backgroud.svelte
@@ -0,0 +1,20 @@
+<script></script>
+
+<!-- components -->
+<figure class="bg-image-1 bg-overlay bg-overlay-300">
+  <img src="/images/illustration/people2.png" alt="" />
+</figure>
+
+<!-- components -->
+
+<style>
+  .bg-image-1 {
+    position: fixed;
+    top: 10%;
+    left: 55%;
+    transform: scale(0.8);
+    -webkit-transform-origin: 50% 50%;
+    transform-origin: 50% 50%;
+    z-index: -1;
+  }
+</style>
